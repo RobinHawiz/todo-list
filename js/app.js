89db910f29f49ffa04de6cc7335e4af6bbe30348
robinhawiz <Robin.Hawiz.6670@student.uu.se>
2020-06-03 17:40:04 +0200

    working with local storage

--- js/app.js
+++ js/app.js
@@ -79,3 +79,14 @@ function filterTodo(e) {
     }
   });
 }
+
+function saveLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
